9bdf4eb1d80fda0107d9267f34d2e60a7d6d1e3b
gk-krajewski <gkkrajewski@gmail.com>
2026-03-11 18:40:37 +0100

    wyszukiwanie po id

--- src/Controller/PetController.php
+++ src/Controller/PetController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Service\PetstoreClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,9 +17,27 @@ final class PetController extends AbstractController
     }
 
     #[Route('/pet/show', name: 'pet_show', methods: ['GET'])]
-    public function show(): Response
+    public function show(Request $request, PetstoreClient $petstoreClient): Response
     {
-        return new Response('Tutaj będzie podgląd.');
+        $id = $request->query->getInt('id');
+
+        if ($id <= 0) {
+            $this->addFlash('error', 'Podano nieprawidłowe ID.');
+
+            return $this->redirectToRoute('pet_index');
+        }
+
+        $pet = $petstoreClient->getPetById($id);
+
+        if ($pet === null) {
+            $this->addFlash('error', 'Nie znaleziono.');
+
+            return $this->redirectToRoute('pet_index');
+        }
+
+        return $this->render('pet/show.html.twig', [
+            'pet' => $pet,
+        ]);
     }
 
     #[Route('/pet/create', name: 'pet_create', methods: ['GET', 'POST'])]
