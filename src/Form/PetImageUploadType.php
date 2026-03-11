a9fc06f303c83ac958404be4c2d3948e8f09d072
gk-krajewski <gkkrajewski@gmail.com>
2026-03-11 21:39:19 +0100

    dostosowanie formularza uploadu obrazu do użycia w create i edit

--- src/Form/PetImageUploadType.php
+++ src/Form/PetImageUploadType.php
@@ -7,21 +7,35 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class PetImageUploadType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $embedded = $options['embedded'];
+
         $builder
             ->add('additionalMetadata', TextType::class, [
-                'label' => 'Dodatkowe metadane',
+                'label' => 'Dodatkowe metadane obrazu',
                 'required' => false,
             ])
             ->add('image', FileType::class, [
-                'label' => 'Obraz',
-            ])
-            ->add('save', SubmitType::class, [
+                'label' => 'Plik obrazu',
+                'required' => false,
+            ]);
+
+        if (!$embedded) {
+            $builder->add('save', SubmitType::class, [
                 'label' => 'Prześlij obraz',
             ]);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'embedded' => false,
+        ]);
     }
 }
