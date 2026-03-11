5fceb97a8cdc349128ba119f1d080f18b68a8039
gk-krajewski <gkkrajewski@gmail.com>
2026-03-11 18:37:59 +0100

    dodanie klienta api petstore

---
+++ src/Service/PetstoreClient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+final class PetstoreClient
+{
+    public function __construct(
+        private readonly HttpClientInterface $httpClient,
+        private readonly string $petstoreApiBaseUrl,
+    ) {
+    }
+
+    public function getPetById(int $id): ?array
+    {
+        $response = $this->httpClient->request(
+            'GET',
+            sprintf('%s/pet/%d', $this->petstoreApiBaseUrl, $id)
+        );
+
+        if ($response->getStatusCode() === 404) {
+            return null;
+        }
+
+        return $response->toArray(false);
+    }
+}
